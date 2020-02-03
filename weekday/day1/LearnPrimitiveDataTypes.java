624d6785e6a83b9179c53d98def55b497e64fb6b
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-02-03 14:58:43 +0530

    Add files via upload

---
+++ weekday/day1/LearnPrimitiveDataTypes.java
@@ -0,0 +1,52 @@
+package weekday1.day1;
+
+public class LearnPrimitiveDataTypes {
+
+	public static void main(String[] args) {
+
+		// Declaring a Character
+		
+		char a = '$';
+		System.out.println(a);
+		
+		// Declaring a boolean
+		
+		boolean flag  = true;
+		System.out.println(flag);
+		
+		// Declaring a integer
+		
+		int b = 212334566;
+		System.out.println(b);
+		
+		//Declaring a byte
+		
+		byte c = 34;
+		System.out.println(c);
+		
+		//Declaring a Long
+		
+		long d = 701030974567881L;
+		
+		//Declaring a short
+		
+		short e =4000;
+		System.out.println(e);
+		
+		//Declaring a Float
+		
+		float f =1223.3122f;
+		System.out.println(f);
+		
+		//Declaring a double
+		
+		double g = 1234.5657232443457;
+		System.out.println(g);
+		
+		String name = "Naveen";
+		System.out.println(name);
+
+
+	}
+
+}
---
+++ weekday/day1/Mobile.java
@@ -0,0 +1,38 @@
+package weekday1.day1;
+
+
+public class Mobile {
+
+	// Syntax to create method : AccessSpecifier Returntype MethodName{}
+
+	public void brandName() {
+
+		System.out.println("Apple");
+
+	}
+
+	public void mobileColour() {
+
+		System.out.println("Black");
+	}
+
+	public void camera() {
+
+		System.out.println("Click Picture");
+	}
+	
+	/*
+	 * public static void main(String[]args) {
+	 * 
+	 * // Syntax to create an object for the class // ClassName objectName = new
+	 * ClassName();
+	 * 
+	 * Mobile phone = new Mobile(); phone.brandName(); phone.camera();
+	 * phone.mobileColour();
+	 * 
+	 * 
+	 * }
+	 */
+
+}
+
